bd8c8032467b7c4f8bb83c7304c59ffaec5c6004
Jan Schnasse <jan@schnasse.eu>
2016-04-27 10:27:36 +0200

    Fix: display of files in parent resources - hide fielInfo fields - remove field overrides from theme.js

--- theme.js
+++ theme.js
@@ -5,27 +5,6 @@
   Drupal.behaviors.edoweb_drupal_theme_entity_minimize = {
     attach: function (context, settings) {
 
-    var fieldLabel = 'Verbund-ID';
-    var htattr = $('.field-name-field-edoweb-parallel .resolved', context).attr('data-curie');
-    var htnr = '';
-    if( htattr ){
-      htnr = htattr.replace('lr:','');
-    };
-
-
-    $('.field-name-field-edoweb-parallel .field-label', context).text(fieldLabel);
-    $('.field-name-field-edoweb-parallel', context).ajaxComplete(function() {
-      //$(this).find('.field-label').text('HT-Nummer');
-      $(this).find('table').remove();
-      $('.field-name-field-edoweb-parallel').once().append( '<div class="field-items"><div class="field-item even">'
-         + '<a target="_blank" href="http://193.30.112.134/F/?func=find-c&ccl_term=IDN%3D' + htnr + '"'
-         + ' data-target-bundle="monograph" data-curie="lr:' + htnr + '" resource="http://lobid.org/resource/' + htnr + '" class="resolved">'
-         + htnr +'</a>'
-         + '</div></div>');
-      $('.field-name-field-edoweb-urn').after($(this));
-
-      });
-
 
     }
   };
