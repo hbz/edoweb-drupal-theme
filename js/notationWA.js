e19908e772c5e6967857ed34893f606185972d10
Andres Quast <quast@hbz-nrw.de>
2016-07-17 14:30:08 +0200

    corrected layout

--- js/notationWA.js
+++ js/notationWA.js
@@ -8,7 +8,7 @@
       $('.field-name-field-edoweb-rpb-subject .field-item').each(function() {
         var notationAttr = $(this).find('a.resolved').attr('data-curie').replace(/https:\/\/w3id.org\/lobid\/rpb2#n/,'')
         .replace(/:n/, '');
-        $(this).prepend('<span class="lbznotation">' + notationAttr + '</span>');
+        $(this).prepend('<div class="lbznotation">' + notationAttr + '</div>');
     
       });
     }
