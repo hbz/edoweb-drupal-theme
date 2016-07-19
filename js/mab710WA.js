c82482b776fe168b32f7a79bafda54f13737a5cf
Andres Quast <quast@hbz-nrw.de>
2016-07-19 17:49:47 +0200

    start implement toggle feature

--- js/mab710WA.js
+++ js/mab710WA.js
@@ -3,8 +3,9 @@
     Drupal.behaviors.edoweb_drupal_theme_entity_minimize = {
     attach: function (context, settings) {
 
-      $('.field-name-field-edoweb-subject-chain .field-item').once(function() {
-      $('.field-name-field-edoweb-field-mab710').hide();
+      $('.field-name-field-edoweb-subject-chain .field-item', context).once(function() {
+        $(this).append('<div class="field-item toggleButton">mehr&nbsp;<span class="octicon octicon-triangle-right"</div>');
+        $('.field-name-field-mab-710').hide();
         
       });
 
