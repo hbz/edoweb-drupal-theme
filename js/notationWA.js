31881f29e0429246420793e090c93653901ed06a
edoweb-test <edoweb-admin@hbz-nrw.de>
2017-06-29 09:43:13 +0200

    hide all subject-URIs that contain rpb#nr as substring - remove leading n before rpb notation numbering

--- js/notationWA.js
+++ js/notationWA.js
@@ -3,8 +3,15 @@
     attach: function (context, settings) {
       $('.rpbSubject a[title]').each(function() {
         var notationAttr = $(this).attr('href');
-	var result = notationAttr.replace(/^.*rpb.?%23/,'').replace(/.$/, "");
-        $(this).prepend(result);    
+	var result = notationAttr.replace(/^.*rpb.?%23n?/,'').replace(/.$/, "");
+	  if(notationAttr.indexOf("rpb%23nr") == -1){
+            $(this).prepend(result);    
+	}else{
+	    // We do not show URIs that contain rpb#nr 
+	    // because the LBZ does not want them to show up
+	    // in repository
+	    $(this).parents("li").hide();
+	}
       });
     }
   };
